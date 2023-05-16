6425ae835ffc0311f0a483ae182e2b98042224fd
ioanacioata02 <ioana.larisa.29@gmail.com>
2023-05-16 13:00:20 +0300

    update

--- Optional10/ServerApplication/src/main/java/ClientThread.java
+++ Optional10/ServerApplication/src/main/java/ClientThread.java
@@ -18,9 +18,9 @@ public class ClientThread extends Thread {
 
     public void run() {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(player.getSocket().getInputStream()));
-             PrintWriter out = new PrintWriter(player.getSocket().getOutputStream())){
+             PrintWriter out = new PrintWriter(player.getSocket().getOutputStream())) {
             while (running) {
-                while (!in.ready()){
+                while (!in.ready()) {
 
                 }
                 String request = in.readLine();
@@ -61,10 +61,8 @@ public class ClientThread extends Thread {
                 gameServer.addGame(new Game());
             }
             player.notify("new Game created");
-        }
-
-        else if (request.equals("join game")) {
-            if(player.inGame()) {
+        } else if (request.equals("join game")) {
+            if (player.inGame()) {
                 player.notify("Already joined a game");
                 return;
             }
@@ -77,27 +75,25 @@ public class ClientThread extends Thread {
             if (game == null)
                 player.notify("no available game. try to create one");
 
-        }
-        else if (request.startsWith("submit move")) {
-            if(!player.inGame())
+        } else if (request.startsWith("submit move")) {
+            if (!player.inGame())
                 player.notify("Join a game first!");
-            else
-            {
+            else {
                 Pattern pattern = Pattern.compile("\\d+");
                 Matcher matcher = pattern.matcher(request);
                 int k = 0;
                 int[] coord = new int[2];
                 while (matcher.find()) {
-                    if(k == 2) {
+                    if (k == 2) {
                         player.notify("Comanda invalida");
                         return;
                     }
-                    coord[k] = Integer.parseInt(matcher.group());
+                    String result = matcher.group();
+                    coord[k++] = Integer.parseInt(result);
                 }
                 player.submitMove(coord[0], coord[1]);
             }
-            }
-        else
+        } else
             player.notify("Comanda invalida");
     }
 }
